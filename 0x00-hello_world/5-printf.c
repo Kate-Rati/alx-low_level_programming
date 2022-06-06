a71061fc48cdd244b534abefe5a1157daededa05
Kate-Rati <katedzaratidzo@gmail.com>
2022-06-06 13:04:50 -0700

    fix 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
+#!/bin/bash
 #include<stdio.h>
-/**
-*main -main block
-*Return: 0
+ /**
+*main -method here
+*returns: 0
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
-return (0);
+printf ("with proper grammar, but the outcome is a piece of art,
+");
+return(0);
 }
