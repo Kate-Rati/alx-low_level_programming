2ff8501c7a35e229594fcec7cdcbfa13b99b0b89
Kate-Rati <katedzaratidzo@gmail.com>
2022-06-06 13:27:47 -0700

    advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include<stdio.h>
+ #include <unistd.h>
+/**
+*main -method here
+*returns: 1
+*/
+int main(void)
+{
+char ch[]="and that piece of art is useful" - Dora Korpar, 2015-10-19
+";
+write(2, ch sizeof(ch))
+return(1);
+}
