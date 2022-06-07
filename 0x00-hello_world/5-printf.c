cb952baa2768ed198145a730fc1889a57af952ee
Kate-Rati <katedzaratidzo@gmail.com>
2022-06-07 04:54:22 -0700

    Correction - 5-printf.c: fixed typos

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
-#!/bin/bash
 #include<stdio.h>
- /**
+/**
 *main -method here
 *returns: 0
 */
 int main(void)
 {
-printf ("with proper grammar, but the outcome is a piece of art,
-");
-return(0);
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
