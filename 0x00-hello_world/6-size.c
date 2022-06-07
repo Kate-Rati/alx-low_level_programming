8bcdf34ca288c626a10a3d2084ab3699ada94a7d
Kate-Rati <katedzaratidzo@gmail.com>
2022-06-07 05:38:12 -0700

    Corrections on 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,16 @@
-#!/bin/bash
-#include<stdio.h
+#include<stdio.h>
+/**
+ * main - execution begins here
+ *
+ * Return: 0 on successful execution
+ */
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long: %lu byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
+	printf("Size of a char: %lu byte(s)\n", sizeof(float));
+
+	return (0);
+}
